e390cdf731a71a543eb59ca536ff86b9996fa143
Juan Castaño <castano21juan@gmail.com>
2026-05-24 17:41:54 -0500

    Paso 17: FormVentas con registro de ventas y cambio de estado automatico

--- Catalogo de Vehiculo/Domain/Entities/Vehiculo.cs
+++ Catalogo de Vehiculo/Domain/Entities/Vehiculo.cs
@@ -3,6 +3,7 @@
 {
     public abstract class Vehiculo
     {
+        public int Id { get; set; }
         public string Marca { get; set; } = "";
         public string Modelo { get; set; } = "";
         public int Año { get; set; }
